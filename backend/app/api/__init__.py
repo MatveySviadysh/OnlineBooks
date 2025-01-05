d5661eaf2b3635741c22a1728d8b1b7ceb4aadba
MatveySviadysh <mkomp06@gmail.com>
2025-01-05 13:10:34 +0300

    rename file router

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .user.router import router as user_router
+from .user.user_api import router as user_router
 
 router = APIRouter()
 router.include_router(user_router, prefix="/users", tags=["users"])
--- backend/app/api/user/user_api.py
+++ backend/app/api/user/user_api.py
@@ -1,13 +1,23 @@
-@app.post("/register")
+from fastapi import APIRouter, Depends, Response, Request
+from schemas.schemas import UserCreate, UserLogin, UserResponse
+from services.services import create_user, authenticate_user, get_current_user, logout
+from db.models.user import User
+
+router = APIRouter()
+
+@router.post("/register")
 async def register(user: UserCreate):
     return create_user(user)
 
-# Войти в систему
-@app.post("/login")
-async def login(user: UserLogin):
-    return authenticate_user(user)
+@router.post("/login")
+async def login(user: UserLogin, response: Response):
+    return authenticate_user(user, response)
 
-# Выйти из системы (это просто пример, без реальной аутентификации с токеном)
-@app.post("/logout/")
-async def logout():
-    return {"message": "Logged out successfully"}
+@router.post("/logout")
+async def logout_user(response: Response):
+    return logout(response)
+
+@router.get("/me")
+async def get_user_me(request: Request):
+    user_email = get_current_user(request)
+    return {"email": user_email}
