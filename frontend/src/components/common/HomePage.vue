99c1d9d9dc0f35a4513374482595fa9363f60bf0
MatveySviadysh <mkomp06@gmail.com>
2025-01-07 14:01:22 +0300

    add map

--- frontend/src/components/common/HomePage.vue
+++ frontend/src/components/common/HomePage.vue
@@ -3,12 +3,27 @@
     <img src="@/assets/main.jpg" alt="">
     <BookTicker />
     <BooksList />
+    <div class="map-container">
+      <h2>Локация</h2>
+      <l-map
+        style="height: 400px; width: 100%;"
+        :zoom="zoom"
+        :center="center"
+      >
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <l-marker :lat-lng="marker"></l-marker>
+      </l-map>
+    </div>
     <CookieConsent />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { LMap, LTileLayer, LMarker } from "vue3-leaflet";
+import "leaflet/dist/leaflet.css";
 import '@/styles/components/common/HomePage.scss'
 import BookTicker from '../help/BookTicker.vue'
 import CookieConsent from '../help/CookieConsent.vue'
@@ -19,7 +34,29 @@ export default defineComponent({
   components: {
     BookTicker,
     CookieConsent,
-    BooksList
+    BooksList,
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  setup() {
+    const center = ref<[number, number]>([51.505, -0.09]); // Координаты центра карты
+    const zoom = ref(13); // Зум карты
+    const marker = ref<[number, number]>([51.505, -0.09]); // Координаты маркера
+
+    return {
+      center,
+      zoom,
+      marker
+    };
   }
-})
-</script> 
+});
+</script>
+
+<style scoped>
+.map-container {
+  margin: 2rem 0;
+  text-align: center;
+}
+</style>
+
